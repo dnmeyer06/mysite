b065c8ba3070bb21282d9d6f12aa1d9c9b113dff
dnmeyer06 <dnmeyer06@gmail.com>
2016-07-29 09:15:35 -0400

    Updating Photo subpage

--- photo/app.js
+++ photo/app.js
@@ -1,5 +1,6 @@
 
 $( "#slide" ).click(function() {
 	$( ".gallery").css("display", "block");
+	$( ".cap").css("display", "none");
 	$( "#landing" ).animate({ "top": "-=80vh" }, 900).delay(400);
 });
