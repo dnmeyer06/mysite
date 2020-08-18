faec377f87dd726ba69488821e96c6c81d480594
dnmeyer06 <dnmeyer06@gmail.com>
2020-08-18 17:04:31 +0800

    Optimizing images and styling

--- src/templating/template.js
+++ src/templating/template.js
@@ -1,7 +1,8 @@
 const workTemplate =
   "{{#workExperience}}" +
-  '<div class="work">' +
-  '<a class="{{logo}}" href="{{url}}" target="_blank"> </a >' +
+  '<div class="work {{prefix}}-work">' +
+  '<a class="{{prefix}}-logo" href="{{url}}" target="_blank"> </a >' +
+  '<h2 class="position">{{position}}</h2>' +
   "{{ #contentBlocks }}" +
   '<div class="content-block">' +
   '<img src="{{imgSrc}}" alt="" />' +
@@ -24,8 +25,29 @@ const workTemplate =
 const workData = {
   workExperience: [
     {
-      logo: "pp-logo",
       url: "https://prodperfect.com/",
+      position: "Product Engineer",
+      prefix: "pp",
+      contentBlocks: [
+        {
+          imgSrc: "./images/pp/pp-1.png",
+          headerPoint: "Built tools that benefited customer experience",
+          bodyPoint:
+            "Contributed to architecting and developing, from the ground up, a web app that gave customers insight into how our service was benefiting their development efforts.",
+        },
+        {
+          imgSrc: "./images/pp/pp-2.png",
+          headerPoint: "PP Point 2",
+          bodyPoint:
+            "Built internal CI system to serve as an operations hub to reduce overhead costs of external services and increase the efficiency of customer support teams.",
+        },
+        {
+          imgSrc: "./images/pp/pp-3.png",
+          headerPoint: "PP Point 3",
+          bodyPoint:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
+        },
+      ],
       tools: [
         { tool: "react" },
         { tool: "AWS" },
@@ -37,31 +59,11 @@ const workData = {
         { tool: "Jest" },
         { tool: "Ant-Design-React-UI" },
       ],
-      contentBlocks: [
-        {
-          imgSrc: "./images/pp/pp-1.png",
-          headerPoint: "PP Point 1",
-          bodyPoint:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
-        },
-        {
-          imgSrc: "./images/pp/pp-2.png",
-          headerPoint: "PP Point 2",
-          bodyPoint:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
-        },
-        {
-          imgSrc: "./images/pp/pp-3.png",
-          headerPoint: "PP Point 3",
-          bodyPoint:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
-        },
-      ],
     },
     {
-      logo: "mm-logo",
       url: "https://www.massmutual.com/",
-      tools: [{ tool: "React" }, { tool: "SASS" }, { tool: "Cloud-CMS" }],
+      position: "Software Engineer",
+      prefix: "mm",
       contentBlocks: [
         {
           imgSrc: "./images/massmutual/mm-1.png",
@@ -82,30 +84,32 @@ const workData = {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
         },
       ],
+      tools: [{ tool: "React" }, { tool: "SASS" }, { tool: "Cloud-CMS" }],
     },
     {
-      logo: "epfr-logo",
       url:
         "https://financialintelligence.informa.com/products-and-services/data-analysis-and-tools/epfr",
-      tools: [
-        { tool: "Vue.js" },
-        { tool: "SASS" },
-        { tool: "Jquery" },
-        { tool: "Adobe-Illustrator" },
-      ],
+      position: "UI Engineer",
+      prefix: "epfr",
       contentBlocks: [
         {
-          imgSrc: "./images/epfr/epfr-1.jpg",
+          imgSrc: "./images/epfr/epfr-1.png",
           headerPoint: "EPFR Point 1",
           bodyPoint:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
         },
         {
-          imgSrc: "./images/epfr/epfr-2.jpg",
+          imgSrc: "./images/epfr/epfr-2.png",
           headerPoint: "EPFR Point 2",
           bodyPoint:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sedvehicula lacinia placerat. Aliquam dapibus consectetur nibh quis lacinia. Integer eleifend nec risus at dignissim. Mauris a posuere erat, nec luctus tellus. Donec quis tincidunt mi, eget lobortis odio. Donec at feugiat mauris. Vestibulum mollis, enim id lobortis malesuada, dui dolor.",
         },
+      ],
+      tools: [
+        { tool: "Vue.js" },
+        { tool: "SASS" },
+        { tool: "Jquery" },
+        { tool: "Adobe-Illustrator" },
       ],
     },
   ],
